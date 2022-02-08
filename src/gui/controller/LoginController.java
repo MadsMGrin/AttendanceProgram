1e4868c9c02029550dfb63e2cb120410b9543ef3
Renaldinho <rena0968@easv365.dk>
2022-02-08 10:41:56 +0100

    sceneSwitch

--- src/gui/controller/LoginController.java
+++ src/gui/controller/LoginController.java
@@ -1,4 +1,44 @@
 package gui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class LoginController {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private TextField usernameTxt;
+
+    @FXML
+    private PasswordField passwordTxt;
+
+    @FXML
+    void handleCancel(ActionEvent event) throws IOException {
+        ((Stage) root.getScene().getWindow()).close();
+
+        Parent newRoot = FXMLLoader.load(getClass().getResource("../view/MainView.fxml"));
+        Scene scene = new Scene(newRoot);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void handleOk(ActionEvent event) {
+
+    }
+
 }
+
--- src/gui/controller/MainController.java
+++ src/gui/controller/MainController.java
@@ -1,4 +1,44 @@
 package gui.controller;
 
+import com.sun.tools.javac.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainController {
+
+    @FXML
+    private AnchorPane root;
+
+    Main mainApp;
+
+    public void setMainApp(Main mainApp){
+        this.mainApp = mainApp;
+    }
+
+    public void handleStudent(ActionEvent actionEvent) throws IOException {
+        openLoginWindow();
+    }
+
+    public void handleTeacher(ActionEvent actionEvent) throws IOException {
+        openLoginWindow();
+    }
+
+    private void openLoginWindow() throws IOException {
+        Parent newRoot = FXMLLoader.load(getClass().getResource("../view/LoginView.fxml"));
+        Scene scene = new Scene(newRoot);
+        Stage newStage = new Stage();
+        newStage.setScene(scene);
+        newStage.show();
+
+        Stage stage = ((Stage) root.getScene().getWindow());
+        stage.close();
+    }
 }
