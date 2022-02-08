918d3bfe91e6bbf08921b883d340af0616138b83
DoncusSpelunkus <90444167+DoncusSpelunkus@users.noreply.github.com>
2022-02-08 12:19:33 +0100

    Create Classes.java

---
+++ src/BE/Classes.java
@@ -0,0 +1,28 @@
+package BE;
+
+public class Classes {
+    private String name;
+    private int amount;
+
+    public Classes(String name, int amount){
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+}
