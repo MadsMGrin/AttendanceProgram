63e44eaf20f28d5b999eb88d7de9364f13d20a8e
Filip <91468087+Neshi24@users.noreply.github.com>
2022-02-22 18:42:01 +0100

    Lists

--- src/BLL/AttendanceManager.java
+++ src/BLL/AttendanceManager.java
@@ -1,4 +1,35 @@
 package BLL;
 
+import BE.Classes;
+import BE.Student;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.List;
+
 public class AttendanceManager implements AttendanceFacade {
+
+    public List<Classes> getAllClasses() {
+        //
+        ObservableList<Classes> ClassesList;
+        ClassesList = FXCollections.observableArrayList();
+        ClassesList.add(new Classes("klassa1", 2131,"JEPO", null));
+        ClassesList.add(new Classes("klassa2", 2138956781,"JEPO", null));
+        ClassesList.add(new Classes("klassa3", 2678,"JEPO", null));
+        //
+
+        return ClassesList;
+    }
+    public List<Student> getAllStudent() {
+
+        //
+        ObservableList<Student> StudentList;
+        StudentList = FXCollections.observableArrayList();
+        StudentList.add(new Student("marek", "DADADA@xD?.Xd?", "klassa1", 5.5));
+        StudentList.add(new Student("szparek","eqwe@XDD.com", "klassa2", 3.3));
+        StudentList.add(new Student("IMGAY","DAD@xd.com","klassa2", 6.6));
+        //
+        return StudentList;
+    }
+
 }
--- src/gui/controller/TeacherHubController.java
+++ src/gui/controller/TeacherHubController.java
@@ -1,6 +1,7 @@
 package gui.controller;
 
 import BE.Classes;
+import BE.Student;
 import BE.Teacher;
 import gui.Model.MainModel;
 import javafx.fxml.FXML;
@@ -14,20 +15,21 @@ public class TeacherHubController {
     @FXML
     private TableColumn <Classes, String> classNameColumn,teacherColumn, classSizeColumn;
     @FXML
+    private TableColumn <Student, String> studentNameColumn, absenceColumn;
+    @FXML
     private TableView<Classes> tableView;
     @FXML
     private TableView tableViewStudent;
-
-    public TeacherHubController() {
-    }
 
 
     public void initialize() {
         try {
             tableViewProperty();
+            tableViewStudentProperty();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
 
     }
 
@@ -35,9 +37,12 @@ public class TeacherHubController {
         classNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         classSizeColumn.setCellValueFactory(new PropertyValueFactory<>("amount"));
         teacherColumn.setCellValueFactory(new PropertyValueFactory<>("teacher"));
-        tableView.setItems(mainModel.getClassesObservableList());
+        tableView.setItems(mainModel.getAllClasses());
+    }
+    private  void tableViewStudentProperty(){
+        studentNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        absenceColumn.setCellValueFactory(new PropertyValueFactory<>("absence"));
+        tableViewStudent.setItems(mainModel.getAllStudent());
     }
 
 }
-//teacherColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-//tableView.setItems(mainModel.getTeacherObservableList());
