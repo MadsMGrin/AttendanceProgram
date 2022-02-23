ffa25649fdeaf661c753027c576da372a3649a1e
DoncusSpelunkus <90444167+DoncusSpelunkus@users.noreply.github.com>
2022-02-23 11:10:21 +0100

    fixed lists

--- src/DAL/AttandenceDAL.java
+++ src/DAL/AttandenceDAL.java
@@ -11,8 +11,10 @@ import java.util.List;
 public class AttandenceDAL {
 
     private List<String> listOfClasses;
+    private List<Student> listOfStudents;
 
     private void makeClassList(){
+        listOfStudents.add(new Student("Renars", "Renars.com", "English", 0.5));
         listOfClasses.add("English");
         listOfClasses.add("Computer Science");
         listOfClasses.add("Literature");
@@ -35,35 +37,35 @@ public class AttandenceDAL {
         studentData.add(new Student("Lydia", "Kunz", "Math", 7));
         studentData.add(new Student("Anna", "Best", "Latin", 15));
         studentData.add(new Student("Stefan", "Meier", "Physics", 4));
-        studentData.add(new Student("Martin", "Mueller", "German", 1);
+        studentData.add(new Student("Martin", "Mueller", "German", 1));
         return studentData;
     }
 
     public ObservableList<Teacher> getTeacherData() {
         ObservableList<Teacher> teacherData = FXCollections.observableArrayList();
         teacherData.add(new Teacher("Hans", "Muster", listOfClasses));
-        teacherData.add(new Teacher("Jeppe", "Moritz", "3"));
-        teacherData.add(new Teacher("Peter", "Stegger", "3"));
-        teacherData.add(new Teacher("Trine", "Hansen", "3"));
-        teacherData.add(new Teacher("Magnus", "Skaregaard", "3"));
-        teacherData.add(new Teacher("Laura", "Karlsen", "3"));
-        teacherData.add(new Teacher("Marcus", "Madsen", "3"));
-        teacherData.add(new Teacher("Lars", "Lindgren", "3"));
-        teacherData.add(new Teacher("Mads", "Stefansson", "3"));
+        teacherData.add(new Teacher("Jeppe", "Moritz", listOfClasses));
+        teacherData.add(new Teacher("Peter", "Stegger", listOfClasses));
+        teacherData.add(new Teacher("Trine", "Hansen", listOfClasses));
+        teacherData.add(new Teacher("Magnus", "Skaregaard", listOfClasses));
+        teacherData.add(new Teacher("Laura", "Karlsen", listOfClasses));
+        teacherData.add(new Teacher("Marcus", "Madsen", listOfClasses));
+        teacherData.add(new Teacher("Lars", "Lindgren", listOfClasses));
+        teacherData.add(new Teacher("Mads", "Stefansson", listOfClasses));
         return teacherData;
     }
 
     public ObservableList<Classes> getClassesData() {
         ObservableList<Classes> classesData = FXCollections.observableArrayList();
-        classesData.add(new Classes("English", 3, "4"));
-        classesData.add(new Classes("Computer Science", 2, "2"));
-        classesData.add(new Classes("Litareture", 1, "2"));
-        classesData.add(new Classes("Spanish", 2, "4"));
-        classesData.add(new Classes("Geometry", 3, "4"));
-        classesData.add(new Classes("Latin", 4, "4"));
-        classesData.add(new Classes("Math", 4, "5"));
-        classesData.add(new Classes("Physics", 2, "2"));
-        classesData.add(new Classes("German", 1, "2"));
+        classesData.add(new Classes("English", 3, "4", listOfStudents));
+        classesData.add(new Classes("Computer Science", 2, "2", listOfStudents));
+        classesData.add(new Classes("Litareture", 1, "2", listOfStudents));
+        classesData.add(new Classes("Spanish", 2, "4", listOfStudents));
+        classesData.add(new Classes("Geometry", 3, "4", listOfStudents));
+        classesData.add(new Classes("Latin", 4, "4", listOfStudents));
+        classesData.add(new Classes("Math", 4, "5", listOfStudents));
+        classesData.add(new Classes("Physics", 2, "2", listOfStudents));
+        classesData.add(new Classes("German", 1, "2", listOfStudents));
         return classesData;
     }
 
