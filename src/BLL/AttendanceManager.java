a287713f03c7007657a1232b8b7da9bc25ba7caf
DoncusSpelunkus <90444167+DoncusSpelunkus@users.noreply.github.com>
2022-02-23 21:54:28 +0100

    connection to dal

--- src/BLL/AttendanceManager.java
+++ src/BLL/AttendanceManager.java
@@ -2,6 +2,8 @@ package BLL;
 
 import BE.Classes;
 import BE.Student;
+import DAL.DALInterface;
+import DAL.DALManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -10,48 +12,19 @@ import java.util.List;
 
 public class AttendanceManager implements AttendanceFacade {
 
+    private DALInterface dalInterface;
+
+    public AttendanceManager(){
+        dalInterface = new DALManager();
+    }
+
     public List<Classes> getAllClasses() {
-        //TODO we need to move it to DAL
-        //
-        ObservableList<Student> StudentList1;
-        StudentList1 = FXCollections.observableArrayList();
-        StudentList1.add(new Student("marek1", "DADADA@xD?.Xd?", "klassa1", 5.5));
-        StudentList1.add(new Student("szparek1","eqwe@XDD.com", "klassa1", 3.3));
-        ObservableList<Student> StudentList2;
-        StudentList2 = FXCollections.observableArrayList();
-        StudentList2.add(new Student("marek2", "DADADA@xD?.Xd?", "klassa2", 5.5));
-        StudentList2.add(new Student("szparek2","eqwe@XDD.com", "klassa2", 3.3));
-        ObservableList<Student> StudentList3;
-        StudentList3 = FXCollections.observableArrayList();
-        StudentList3.add(new Student("marek3", "DADADA@xD?.Xd?", "klassa3", 5.5));
-        StudentList3.add(new Student("szparek3","eqwe@XDD.com", "klassa3", 3.3));
-        //
-
-        //
-        ObservableList<Classes> ClassesList;
-        ClassesList = FXCollections.observableArrayList();
-        ClassesList.add(new Classes("klassa1", 2131,"JEPO", StudentList1));
-        ClassesList.add(new Classes("klassa2", 2138956781,"JEPO", StudentList2));
-        ClassesList.add(new Classes("klassa3", 2678,"JEPO", StudentList3));
-        //
-
-        return ClassesList;
+        return dalInterface.getClassesData();
     }
 
 
     public List<Student> getAllStudent() {
-
-        //
-        ObservableList<Student> StudentList;
-        StudentList = FXCollections.observableArrayList();
-        StudentList.add(new Student("marek", "DADADA@xD?.Xd?", "klassa1", 5.5));
-        StudentList.add(new Student("szparek","eqwe@XDD.com", "klassa2", 3.3));
-        StudentList.add(new Student("IMGAY","DAD@xd.com","klassa2", 6.6));
-        StudentList.add(new Student("marek", "DADADA@xD?.Xd?", "klassa1", 5.5));
-        StudentList.add(new Student("szparek","eqwe@XDD.com", "klassa2", 3.3));
-        StudentList.add(new Student("IMGAY","DAD@xd.com","klassa2", 6.6));
-        //
-        return StudentList;
+        return dalInterface.getStudentData();
     }
 
     @Override
